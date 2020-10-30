620ab579e45c81c6f639cc944f4b19fbb5b2ecb0
David Meister <thedavidmeister@gmail.com>
2020-10-30 19:44:48 +0400

    remove request fixtures

--- test/agent_info/signed.ts
+++ test/agent_info/signed.ts
@@ -2,10 +2,11 @@ import { Urls, AgentInfo, AgentInfoPacked } from '../../src/agent_info/info'
 import { AgentInfoSignedRaw, agentInfoSignedRawSafe, agentInfoSignedSafe } from '../../src/agent_info/signed'
 import * as Kitsune from '../../src/kitsune/kitsune'
 import * as Agents from '../fixture/agents'
-import * as Requests from '../fixture/requests'
 import { strict as assert } from 'assert'
 import * as MP from '../../src/msgpack/msgpack'
 import { isRight, isLeft } from 'fp-ts/lib/Either'
+import * as _ from 'lodash'
+import { Ed25519 } from '../../src/crypto/crypto'
 
 describe('agent info signed', () => {
 
@@ -19,14 +20,18 @@ describe('agent info signed', () => {
   assert.ok(isRight(agentInfoSignedRawSafe.decode(MP.encode(Agents.aliceAgentVaporSignedRaw))))
 
   // any bad signature must not be valid
-  assert.ok(isLeft(agentInfoSignedRawSafe.decode(Requests.aliceVaporPutBodyBadSignature)))
+  let badSignature = _.cloneDeep(Agents.aliceAgentVaporSignedRaw)
+  badSignature.signature = new Uint8Array(Array(36)).fill(1)
+  assert.ok(isLeft(agentInfoSignedRawSafe.decode(MP.encode(badSignature))))
  })
 
  it('should decode agentInfoSigned correctly', () => {
   assert.ok(isRight(agentInfoSignedSafe.decode(MP.encode(Agents.aliceAgentVaporSignedRaw))))
 
   // bob must not be allowed to sign alice's info
-  assert.ok(isLeft(agentInfoSignedSafe.decode(Requests.bobSignedAlicePostBody)))
+  let bobSignedAlice = _.cloneDeep(Agents.aliceAgentVaporSignedRaw)
+  bobSignedAlice.signature = Ed25519.sign(bobSignedAlice.agent_info, Agents.bob.secretKey)
+  assert.ok(isLeft(agentInfoSignedSafe.decode(MP.encode(bobSignedAlice))))
  })
 
 })
